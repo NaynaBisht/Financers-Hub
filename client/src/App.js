1c76da2919c2f85c29efce8eaa052656af5568a8
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-30 13:43:03 +0530

    client main files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Navbar from "./components/Navbar"; // Adjust this path based on your structure
+import './index.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingMSME from "./pages/MSME/LandingMSME.js"; 
+import Register from "./pages/MSME/Register.js"; // Ensure the correct path
 
 const App = () => {
     return (
-        <div>
-            <Navbar/>
-            <main>
-            </main>
-        </div>
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Register />} />  {/* Main page for registration */}
+                    <Route path="/msme" element={<LandingMSME />} />  {/* Landing page after sign in */}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
