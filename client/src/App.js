cf2af13bf136304e2d03d5dff471aa75f2b5aced
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-10-01 00:04:50 +0530

    govSchemes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import './index.css'
+import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingMSME from "./pages/MSME/LandingMSME.js"; 
-import Register from "./pages/MSME/Register.js"; // Ensure the correct path
+import Register from "./pages/MSME/Register.js"; 
+import Dashboard from './pages/MSME/Dashboard.js';
+import ApplyLoan from './pages/MSME/ApplyLoan.js';
+import Profile from "./pages/MSME/Profile.js";
+import GovSchemes from "./pages/MSME/GovSchemes.js";
+import FinancialLiteracy from "./pages/MSME/FinancialLiteracy.js";
+import LoanStatus from "./pages/MSME/LoanStatus.js";
+import Support from "./pages/MSME/Support.js";
 
 const App = () => {
     return (
         <Router>
             <div>
                 <Routes>
-                    <Route path="/" element={<Register />} />  {/* Main page for registration */}
-                    <Route path="/msme" element={<LandingMSME />} />  {/* Landing page after sign in */}
+                    <Route path="/" element={<Register />} />  
+                    <Route path="/msme" element={<LandingMSME />} />  
+                    <Route path="/Dashboard" element={<Dashboard />} />  
+                    <Route path="/ApplyLoan" element={<ApplyLoan />} />  
+                    <Route path="/Profile" element={<Profile />} />  
+                    <Route path="/GovSchemes" element={<GovSchemes />} />  
+                    <Route path="/FinancialLiteracy" element={<FinancialLiteracy />} />  
+                    <Route path="/LoanStatus" element={<LoanStatus />} />  
+                    <Route path="/Support" element={<Support />} />  
                 </Routes>
             </div>
         </Router>
