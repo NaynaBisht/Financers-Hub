394fac6536a1968515621571a1f9d6384d47b772
Nayna <145502271+NaynaBisht@users.noreply.github.com>
2025-02-17 03:09:05 +0530

    Routes Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,59 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FirstPage from './pages/FirstPage.js'; // Import the new FirstPage component
-import LandingMSME from "./pages/MSME/LandingMSME.js"; 
-import Register from "./pages/MSME/Register.js"; 
-import Dashboard from './pages/MSME/Dashboard.js';
-import ApplyLoan from './pages/MSME/ApplyLoan.js';
+import FirstPage from "./pages/FirstPage.js"; // Default Landing Page
+import LandingMSME from "./pages/MSME/LandingMSME.js";
+import Register from "./pages/MSME/Register.js";
+import Dashboard from "./pages/MSME/Dashboard.js";
+import ApplyLoan from "./pages/MSME/ApplyLoan.js";
 import Profile from "./pages/MSME/Profile.js";
 import GovSchemes from "./pages/MSME/GovSchemes.js";
 import FinancialLiteracy from "./pages/MSME/FinancialLiteracy.js";
 import LoanStatus from "./pages/MSME/LoanStatus.js";
 import Support from "./pages/MSME/Support.js";
-import Chatbot from "./components/Chatbot.js"
+import Chatbot from "./components/Chatbot.js";
 
-// Import investor pages
+// Import Investor Pages
 import LandingInvestor from "./pages/INVESTOR/LandingInvestor.js";
-import RegisterInvestor from './pages/INVESTOR/RegisterInvestor.js';
-import LoanStatusInvestor from './pages/INVESTOR/LoanStatusInvestor.js';
-import ProfileInvestor from './pages/INVESTOR/ProfileInvestor.js';
-import Supportinv from "./pages/INVESTOR/SupportINV.js";
-import LoanRequests from './pages/INVESTOR/LoanRequests.js';
+import RegisterInvestor from "./pages/INVESTOR/RegisterInvestor.js";
+import LoanStatusInvestor from "./pages/INVESTOR/LoanStatusInvestor.js";
+import ProfileInvestor from "./pages/INVESTOR/ProfileInvestor.js";
+import SupportINV from "./pages/INVESTOR/SupportINV.js";
+import LoanRequests from "./pages/INVESTOR/LoanRequests.js";
 
 const App = () => {
-    return (
-        <Router>
-            <div>
-                <Routes>
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<FirstPage />} />
 
-                   <Route path="/" element={<FirstPage />} />  {/* Set FirstPage as the default route */}
-                   
-                   {/* MSME Routes */}
-                   <Route path="/msme/Register" element={<Register />} />  
-                    <Route path="/msme" element={<LandingMSME />} />  
-                    <Route path="/Dashboard" element={<Dashboard />} />  
-                    <Route path="/ApplyLoan" element={<ApplyLoan />} />  
-                    <Route path="/Profile" element={<Profile />} />  
-                    <Route path="/GovSchemes" element={<GovSchemes />} />  
-                    <Route path="/FinancialLiteracy" element={<FinancialLiteracy />} />  
-                    <Route path="/LoanStatus" element={<LoanStatus />} />  
-                    <Route path="/Support" element={<Support />} />
+          {/* MSME Routes */}
+          <Route path="/msme" element={<LandingMSME />} />
+          <Route path="/msme/register" element={<Register />} />
+          <Route path="/msme/dashboard" element={<Dashboard />} />
+          <Route path="/msme/apply-loan" element={<ApplyLoan />} />
+          <Route path="/msme/profile" element={<Profile />} />
+          <Route path="/msme/gov-schemes" element={<GovSchemes />} />
+          <Route path="/msme/financial-literacy" element={<FinancialLiteracy />} />
+          <Route path="/msme/loan-status/:msmeId" element={<LoanStatus />} />
+          <Route path="/msme/support" element={<Support />} />
 
+          {/* Investor Routes */}
+          <Route path="/investor" element={<LandingInvestor />} />
+          <Route path="/investor/register" element={<RegisterInvestor />} />
+          <Route path="/investor/loan-status/:id" element={<LoanStatusInvestor />} />
+          <Route path="/investor/profile" element={<ProfileInvestor />} />
+          <Route path="/investor/support" element={<SupportINV />} />
+          <Route path="/investor/loan-requests" element={<LoanRequests />} />
+        </Routes>
 
-                   {/* INVESTOR Routes */}
-                    <Route path="/LandingInvestor" element={<LandingInvestor />} /> 
-                    <Route path="/investor/register" element={<RegisterInvestor />} /> 
-                    <Route path="/loanstatus" element={<LoanStatusInvestor />} />  
-                    <Route path="/ProfileInvestor" element={<ProfileInvestor />} />
-                    <Route path="/supportinv" element={<Supportinv />} />
-                    <Route path="/LoanRequests" element={<LoanRequests />} />
-
-                </Routes>
-                <Chatbot/>
-            </div>
-        </Router>
-    );
-}
+        {/* Global Chatbot Component */}
+        <Chatbot />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
