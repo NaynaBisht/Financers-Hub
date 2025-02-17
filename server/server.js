eebe1c0f85d6b3c6fe58e664323824c06679eaa7
Nayna <145502271+NaynaBisht@users.noreply.github.com>
2025-02-17 22:53:04 +0530

    Frontend URL updated

--- server/server.js
+++ server/server.js
@@ -17,11 +17,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 // Middleware
 app.use(express.json()); // JSON Parser
 app.use(express.urlencoded({ extended: true })); // URL Encoded Parser
-app.use(cors({ origin: 'http://localhost:3000', credentials: true })); // CORS
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));// CORS
 
 // Database Connection
 const connectDB = async () => {
