f4af21606518eb9a75c6ea1f90d933e0209b99ec
Nayna <145502271+NaynaBisht@users.noreply.github.com>
2025-02-17 23:38:45 +0530

    cors url update

--- server/server.js
+++ server/server.js
@@ -23,7 +23,8 @@ app.use(express.urlencoded({ extended: true })); // URL Encoded Parser
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:3000', 
+    origin:'https://financers-hub.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials:true
 }
 
