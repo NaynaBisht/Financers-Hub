d7646a90f3e60c235f147742009502c6d2f8c869
Nayna <145502271+NaynaBisht@users.noreply.github.com>
2025-02-17 23:14:09 +0530

    cors url removed

--- server/server.js
+++ server/server.js
@@ -17,30 +17,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 // Middleware
 app.use(express.json()); // JSON Parser
 app.use(express.urlencoded({ extended: true })); // URL Encoded Parser
-
-
-// Allow multiple origins dynamically
-const allowedOrigins = [
-    'https://financers-hub.vercel.app',
-    'https://financers-gu3gi6vir-naynabisht1s-projects.vercel.app', // Add your other frontend origin here
-];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-}));
-
+app.use(cors({ origin: 'http://localhost:3000', credentials: true })); // CORS
 
 // Database Connection
 const connectDB = async () => {
